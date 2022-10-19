1319d41bb48eaa765578cb6b11e08ba3ddd7fe23
ishak-dev <ishak.kazic@gmail.com>
2022-10-19 21:51:25 +0200

    properties-page

--- src/components/properties.js
+++ src/components/properties.js
@@ -1,25 +1,116 @@
 import React from "react";
 import "../style/properties.css";
-import { IconName } from "react-icons/fa";
+import { IconName, FaSearch } from "react-icons/fa";
 import userImg from "../images/profile1.jpg";
 
 const Properties = () => {
   return (
-    <div className="properties-container">
-      <div className="properties-item">
-        <div className="properties-info ">
-          <img className="properties-info-img" src={userImg} />
-          <div className="properties-info-details">
-            <p className="properties-info-username">Ishak Kazic</p>
-            <p className="activity active">Online</p>
+    <div className="properties">
+      <div className="properties-container">
+        <div className="properties-item">
+          <div className="properties-info ">
+            <img className="properties-info-img" src={userImg} />
+            <div className="properties-info-details">
+              <p className="properties-info-username">Ishak Kazic</p>
+              <p className="activity active">Online</p>
+            </div>
+          </div>
+        </div>
+        <div className="properties-item">
+          <div className="properties-info ">
+            <img className="properties-info-img" src={userImg} />
+            <div className="properties-info-details">
+              <p className="properties-info-username">Ishak Kazic</p>
+              <p className="activity active">Online</p>
+            </div>
+          </div>
+        </div>
+        <div className="properties-item">
+          <div className="properties-info ">
+            <img className="properties-info-img" src={userImg} />
+            <div className="properties-info-details">
+              <p className="properties-info-username">Ishak Kazic</p>
+              <p className="activity active">Online</p>
+            </div>
+          </div>
+        </div>
+        <div className="properties-item">
+          <div className="properties-info ">
+            <img className="properties-info-img" src={userImg} />
+            <div className="properties-info-details">
+              <p className="properties-info-username">Ishak Kazic</p>
+              <p className="activity active">Online</p>
+            </div>
+          </div>
+        </div>
+        <div className="properties-item">
+          <div className="properties-info ">
+            <img className="properties-info-img" src={userImg} />
+            <div className="properties-info-details">
+              <p className="properties-info-username">Ishak Kazic</p>
+              <p className="activity offline">Offline</p>
+            </div>
+          </div>
+        </div>
+        <div className="properties-item">
+          <div className="properties-info ">
+            <img className="properties-info-img" src={userImg} />
+            <div className="properties-info-details">
+              <p className="properties-info-username">Ishak Kazic</p>
+              <p className="activity active">Online</p>
+            </div>
+          </div>
+        </div>
+        <div className="properties-item">
+          <div className="properties-info ">
+            <img className="properties-info-img" src={userImg} />
+            <div className="properties-info-details">
+              <p className="properties-info-username">Ishak Kazic</p>
+              <p className="activity active">Online</p>
+            </div>
+          </div>
+        </div>
+        <div className="properties-item">
+          <div className="properties-info ">
+            <img className="properties-info-img" src={userImg} />
+            <div className="properties-info-details">
+              <p className="properties-info-username">Ishak Kazic</p>
+              <p className="activity active">Online</p>
+            </div>
+          </div>
+        </div>
+        <div className="properties-item">
+          <div className="properties-info ">
+            <img className="properties-info-img" src={userImg} />
+            <div className="properties-info-details">
+              <p className="properties-info-username">Ishak Kazic</p>
+              <p className="activity active">Online</p>
+            </div>
+          </div>
+        </div>
+        <div className="properties-item">
+          <div className="properties-info ">
+            <img className="properties-info-img" src={userImg} />
+            <div className="properties-info-details">
+              <p className="properties-info-username">Ishak Kazic</p>
+              <p className="activity active">Online</p>
+            </div>
+          </div>
+        </div>
+        <div className="properties-item">
+          <div className="properties-info ">
+            <img className="properties-info-img" src={userImg} />
+            <div className="properties-info-details">
+              <p className="properties-info-username">Ishak Kazic</p>
+              <p className="activity active">Online</p>
+            </div>
           </div>
         </div>
       </div>
-      <div className="properties-item">2</div>
-      <div className="properties-item">3</div>
-      <div className="properties-item">4</div>
-      <div className="properties-item">5</div>
-      <div className="properties-item">6</div>
+      <div className="search-users-bar">
+        <input type={"text"}></input>
+        <FaSearch className="search-users-btn" />
+      </div>
     </div>
   );
 };
