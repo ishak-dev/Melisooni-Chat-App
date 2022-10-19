9183bf89277151e50e8ee11281dbe5fe439269f3
ishak-dev <ishak.kazic@gmail.com>
2022-10-19 20:23:47 +0200

    propertiese-style

--- src/components/properties.js
+++ src/components/properties.js
@@ -1,11 +1,20 @@
 import React from "react";
 import "../style/properties.css";
 import { IconName } from "react-icons/fa";
+import userImg from "../images/profile1.jpg";
 
 const Properties = () => {
   return (
     <div className="properties-container">
-      <div className="properties-item">1</div>
+      <div className="properties-item">
+        <div className="properties-info ">
+          <img className="properties-info-img" src={userImg} />
+          <div className="properties-info-details">
+            <p className="properties-info-username">Ishak Kazic</p>
+            <p className="activity active">Online</p>
+          </div>
+        </div>
+      </div>
       <div className="properties-item">2</div>
       <div className="properties-item">3</div>
       <div className="properties-item">4</div>
