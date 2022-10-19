bfb5b2a0ae671787d03d1400b2b06c3d4238a6e5
ishak-dev <ishak.kazic@gmail.com>
2022-10-19 19:38:04 +0200

    add-new-pages

---
+++ src/components/properties.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "../style/properties.css";
+import { IconName } from "react-icons/fa";
+
+const Properties = () => {
+  return (
+    <div className="properties-container">
+      <div className="properties-item">1</div>
+      <div className="properties-item">2</div>
+      <div className="properties-item">3</div>
+      <div className="properties-item">4</div>
+      <div className="properties-item">5</div>
+      <div className="properties-item">6</div>
+    </div>
+  );
+};
+
+export default Properties;
