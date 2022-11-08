be757d995453c090d12e20239a86b16a27bcf3fe
Ishak Kazić <ishak.kazic@gmail.com>
2022-11-08 23:36:12 +0100

    create notification page

--- src/components/notification-component.js
+++ src/components/notification-component.js
@@ -1,13 +1,11 @@
 import React from "react";
 import "../style/notification.css"
-const Notification = () => {
+const NotificationComponent = () => {
 
   const [showMessage,setShowMessage]=React.useState(false);
 
 
-  return <div className="notification-section">
-    <h2>Notification</h2>
-    <div className="notification-container">
+  return <div >
       <div className="notification">
       <div className="notification-details">
       <h2>Notification Header</h2><p className="notification-time">11.30</p>
@@ -15,8 +13,7 @@ const Notification = () => {
       <div className="notification-message hide-full-notification"><p>This is test mesage lndas
       dlansld lnas d  ashasjd ahjdhj asd hjashd  ajshd haj ahsjd ha asdh ajshd ja hjash dj j ajsdljldjalsd l lasldn alsnd lnas lnd dlansld lnas d  ashasjd ahjdhj asd hjashd  ajshd haj ahsjd ha asdh ajshd ja hjash dj j ajsdljldjalsd l lasldn alsnd lnas lnd dlansld lnas d  ashasjd ahjdhj asd hjashd  ajshd haj ahsjd ha asdh ajshd ja hjash dj j ajsdljldjalsd l lasldn alsnd lnas lnd dlansld lnas d  ashasjd ahjdhj asd hjashd  ajshd haj ahsjd ha asdh ajshd ja hjash dj j ajsdljldjalsd l lasldn alsnd lnas lnd dlansld lnas d  ashasjd ahjdhj asd hjashd  ajshd haj ahsjd ha asdh ajshd ja hjash dj j ajsdljldjalsd l lasldn alsnd lnas lnd</p></div>
       </div>
-    </div>
   </div>;
 };
 
-export default Notification;
+export default NotificationComponent;
