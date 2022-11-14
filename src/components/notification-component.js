273edb6f60465f89b620641a9726a4339017ec8d
Ishak Kazić <ishak.kazic@gmail.com>
2022-11-14 23:59:38 +0100

    update notification

--- src/components/notification-component.js
+++ src/components/notification-component.js
@@ -4,14 +4,18 @@ const NotificationComponent = () => {
 
   const [showMessage,setShowMessage]=React.useState(false);
 
+  function openMessage(){
+    showMessage(prevState=>!prevState);
+  }
 
   return <div >
       <div className="notification">
       <div className="notification-details">
-      <h2>Notification Header</h2><p className="notification-time">11.30</p>
+      <h3>Notification Header</h3><p className="notification-time">11:30</p>
       </div>
-      <div className="notification-message hide-full-notification"><p>This is test mesage lndas
-      dlansld lnas d  ashasjd ahjdhj asd hjashd  ajshd haj ahsjd ha asdh ajshd ja hjash dj j ajsdljldjalsd l lasldn alsnd lnas lnd dlansld lnas d  ashasjd ahjdhj asd hjashd  ajshd haj ahsjd ha asdh ajshd ja hjash dj j ajsdljldjalsd l lasldn alsnd lnas lnd dlansld lnas d  ashasjd ahjdhj asd hjashd  ajshd haj ahsjd ha asdh ajshd ja hjash dj j ajsdljldjalsd l lasldn alsnd lnas lnd dlansld lnas d  ashasjd ahjdhj asd hjashd  ajshd haj ahsjd ha asdh ajshd ja hjash dj j ajsdljldjalsd l lasldn alsnd lnas lnd dlansld lnas d  ashasjd ahjdhj asd hjashd  ajshd haj ahsjd ha asdh ajshd ja hjash dj j ajsdljldjalsd l lasldn alsnd lnas lnd</p></div>
+      <div className={`notification-message ${!showMessage && "hide-full-notification"}`} onClick={openMessage}><p>This is test mesage lndas
+      dlansld lnas d  ashasjd ahjdhj asd hjashd  ajshd haj ahsjd ha asdh ajshd ja hjash dj j ajsdljldjalsd l lasldn alsnd lnas lnd dlansld lnas d  ashasjd ahjdhj asd hjashd  ajshd haj ahsjd ha asdh ajshd ja hjash dj j ajsdljldjalsd l lasldn alsnd lnas lnd dlansld lnas d  ashasjd ahjdhj asd hjashd  ajshd haj ahsjd ha asdh ajshd ja hjash dj j ajsdljldjalsd l lasldn alsnd lnas lnd dlansld lnas d  ashasjd ahjdhj asd hjashd  ajshd haj ahsjd ha asdh ajshd ja hjash dj j ajsdljldjalsd l lasldn alsnd lnas lnd dlansld lnas d  ashasjd ahjdh das d as das d as da sd as da sd
+      dasdsadasd j asd hjashd  ajshd haj ahsjd ha asdh ajshd ja hjash dj j ajsdljldjalsd l lasldn alsnd lnas lnd</p></div>
       </div>
   </div>;
 };
