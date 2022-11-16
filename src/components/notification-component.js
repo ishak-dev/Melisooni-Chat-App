01d5f84099463cdb23f6e5c84a5f2f09e7c9834c
ishak-dev <ishak.kazic@gmail.com>
2022-11-16 23:32:59 +0100

    fix notificaiton

--- src/components/notification-component.js
+++ src/components/notification-component.js
@@ -1,23 +1,58 @@
 import React from "react";
-import "../style/notification.css"
-const NotificationComponent = () => {
+import "../style/notification.css";
+const NotificationComponent = ({ activityFunc, activeMessageId, id }) => {
+  const [showMessage, setShowMessage] = React.useState(false);
 
-  const [showMessage,setShowMessage]=React.useState(false);
-
-  function openMessage(){
-    showMessage(prevState=>!prevState);
+  function openMessage() {
+    if (activeMessageId == id && showMessage == false) {
+      setShowMessage(true);
+      return activityFunc(id);
+    } else if (activeMessageId == id && showMessage == true) {
+      setShowMessage(false);
+      return activityFunc();
+    } else if (activeMessageId == undefined && showMessage == false) {
+      setShowMessage(true);
+      return activityFunc(id);
+    } else if (activeMessageId != id && showMessage == false) {
+      setShowMessage(true);
+      return activityFunc(id);
+    } else {
+      setShowMessage(false);
+      return activityFunc(id);
+    }
   }
+  console.log(activeMessageId);
+  console.log(showMessage);
 
-  return <div >
-      <div className="notification">
-      <div className="notification-details">
-      <h3>Notification Header</h3><p className="notification-time">11:30</p>
+  return (
+    <div>
+      <div className="notification" onClick={openMessage}>
+        <div className="notification-details">
+          <h3>Notification Header</h3>
+          <p className="notification-time">11:30</p>
+        </div>
+        <div
+          className={`notification-message ${
+            !(activeMessageId === id) && "hide-full-notification"
+          }`}
+        >
+          <p>
+            This is test mesage lndas dlansld lnas d ashasjd ahjdhj asd hjashd
+            ajshd haj ahsjd ha asdh ajshd ja hjash dj j ajsdljldjalsd l lasldn
+            alsnd lnas lnd dlansld lnas d ashasjd ahjdhj asd hjashd ajshd haj
+            ahsjd ha asdh ajshd ja hjash dj j ajsdljldjalsd l lasldn alsnd lnas
+            lnd dlansld lnas d ashasjd ahjdhj asd hjashd ajshd haj ahsjd ha asdh
+            ajshd ja hjash dj j ajsdljldjalsd l lasldn alsnd lnas lnd dlansld
+            lnas d ashasjd ahjdhj asd hjashd ajshd haj ahsjd ha asdh ajshd ja
+            hjash dj j ajsdljldjalsd l lasldn alsnd lnas lnd dlansld lnas d
+            ashasjd ahjdh das d as das d as da sd as da sd dasdsadasd j asd
+            hjashd ajshd haj ahsjd ha asdh ajshd ja hjash dj j ajsdljldjalsd l
+            lasldn alsnd lnas lnd
+          </p>
+        </div>
       </div>
-      <div className={`notification-message ${!showMessage && "hide-full-notification"}`} onClick={openMessage}><p>This is test mesage lndas
-      dlansld lnas d  ashasjd ahjdhj asd hjashd  ajshd haj ahsjd ha asdh ajshd ja hjash dj j ajsdljldjalsd l lasldn alsnd lnas lnd dlansld lnas d  ashasjd ahjdhj asd hjashd  ajshd haj ahsjd ha asdh ajshd ja hjash dj j ajsdljldjalsd l lasldn alsnd lnas lnd dlansld lnas d  ashasjd ahjdhj asd hjashd  ajshd haj ahsjd ha asdh ajshd ja hjash dj j ajsdljldjalsd l lasldn alsnd lnas lnd dlansld lnas d  ashasjd ahjdhj asd hjashd  ajshd haj ahsjd ha asdh ajshd ja hjash dj j ajsdljldjalsd l lasldn alsnd lnas lnd dlansld lnas d  ashasjd ahjdh das d as das d as da sd as da sd
-      dasdsadasd j asd hjashd  ajshd haj ahsjd ha asdh ajshd ja hjash dj j ajsdljldjalsd l lasldn alsnd lnas lnd</p></div>
-      </div>
-  </div>;
+    </div>
+  );
 };
 
 export default NotificationComponent;
--- src/components/notifications.js
+++ src/components/notifications.js
@@ -1,17 +1,31 @@
 import React from "react";
-import "../style/notification.css"
-import NotificationComponent from "./notification-component.js"
+import "../style/notification.css";
+import NotificationComponent from "./notification-component.js";
 const Notification = () => {
+  const [activeMessageId, setActiveMessageId] = React.useState(() => "");
 
-  const [showMessage,setShowMessage]=React.useState(false);
-
-
-  return <div className="notification-section">
-    <h2>Notification</h2>
-    <div className="notification-container">
-      <NotificationComponent />
+  function openMessage(id) {
+    setActiveMessageId(id);
+  }
+  return (
+    <div className="notification-section">
+      <h2>Notification</h2>
+      <div className="notification-container">
+        <NotificationComponent
+          id={2}
+          activeMessageId={activeMessageId}
+          message={"d asd asd asd as "}
+          activityFunc={openMessage}
+        />
+        <NotificationComponent
+          id={3}
+          activeMessageId={activeMessageId}
+          message={"d asd asd asd as "}
+          activityFunc={openMessage}
+        />
+      </div>
     </div>
-  </div>;
+  );
 };
 
 export default Notification;
